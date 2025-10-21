7fa17b9b710335f6c64e13f8c8dfe1b529af7acf
asnounsa <markus@markus.com>
2025-10-21 18:34:45 -0400

    Update files

--- A2/prisma/createsu.js
+++
@@ -1,6 +0,0 @@
-/*
- * Complete this script so that it is able to add a superuser to the database
- * Usage example: 
- *   node prisma/createsu.js clive123 clive.su@mail.utoronto.ca SuperUser123!
- */
-'use strict';

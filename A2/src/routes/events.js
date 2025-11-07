e64bc84452a2e699f529493d13adb70b5f6cb58e
samyasnoun <158087041+samyasnoun@users.noreply.github.com>
2025-11-07 01:17:09 -0500

    Add attachUser before canManageEvent middleware

--- A2/src/routes/events.js
+++ A2/src/routes/events.js
@@ -28,20 +28,20 @@ router.get("/", authenticate, attachUser, getEvents);
 router.get("/:eventId", authenticate, attachUser, getEventById);
 
 // Managers / organizers / superusers can manage event
-router.patch("/:eventId", authenticate, canManageEvent(), patchEventById);
-router.delete("/:eventId", authenticate, canManageEvent(), deleteEventById);
+router.patch("/:eventId", authenticate, attachUser, canManageEvent(), patchEventById);
+router.delete("/:eventId", authenticate, attachUser, canManageEvent(), deleteEventById);
 
-router.post("/:eventId/organizers", authenticate, canManageEvent(), postOrganizerToEvent);
-router.delete("/:eventId/organizers/:userId", authenticate, canManageEvent(), removeOrganizerFromEvent);
+router.post("/:eventId/organizers", authenticate, attachUser, canManageEvent(), postOrganizerToEvent);
+router.delete("/:eventId/organizers/:userId", authenticate, attachUser, canManageEvent(), removeOrganizerFromEvent);
 
-router.post("/:eventId/guests", authenticate, canManageEvent(), postGuestToEvent);
-router.delete("/:eventId/guests/:userId", authenticate, canManageEvent(), deleteGuestFromEvent);
+router.post("/:eventId/guests", authenticate, attachUser, canManageEvent(), postGuestToEvent);
+router.delete("/:eventId/guests/:userId", authenticate, attachUser, canManageEvent(), deleteGuestFromEvent);
 
 // Regular users RSVP to events
 router.post("/:eventId/guests/me", authenticate, attachUser, postCurrentUserToEvent);
 router.delete("/:eventId/guests/me", authenticate, attachUser, removeCurrentUserFromEvent);
 
 // Award points after events
-router.post("/:eventId/transactions", authenticate, canManageEvent(), createRewardTransaction);
+router.post("/:eventId/transactions", authenticate, attachUser, canManageEvent(), createRewardTransaction);
 
 module.exports = router;

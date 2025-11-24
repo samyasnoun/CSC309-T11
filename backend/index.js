6b0dbfed62bb7c0caec99d6a0b73e5dbd45e40a2
samyasnoun <158087041+samyasnoun@users.noreply.github.com>
2025-11-24 01:34:55 -0500

    Setup CORS and load environment variables
    Added CORS and dotenv configuration for environment variables.

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
 import express from "express";
 import routes from "./routes.js";
-// TODO: complete me (loading the necessary packages)
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-// TODO: complete me (CORS)
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+  })
+);
+
 app.use(express.json());
 app.use('', routes);
 
